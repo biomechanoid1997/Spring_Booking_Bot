e49322f6a940c42892da45989be7e621ee95ccba
biomechanoid <biomechanoid1997@yandex.ru>
2024-01-15 23:18:48 +0300

    ...

--- src/main/java/com/example/spring_booking_bot/SpringBookingBotApplication.java
+++ src/main/java/com/example/spring_booking_bot/SpringBookingBotApplication.java
@@ -2,12 +2,21 @@ package com.example.spring_booking_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class SpringBookingBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBookingBotApplication.class, args);
+		try {
+			TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+			telegramBotsApi.registerBot(new Bot());
+		}catch (TelegramApiException e){
+			e.printStackTrace();
+		}
+	}
 	}
 
-}
